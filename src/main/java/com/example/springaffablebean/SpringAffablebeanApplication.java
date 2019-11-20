836c040caca9673a2d577ad9ae5b7127c14f9712
Your Name <you@example.com>
2019-11-20 16:29:10 +0630

    first commit

---
+++ src/main/java/com/example/springaffablebean/SpringAffablebeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaffablebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAffablebeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAffablebeanApplication.class, args);
+    }
+
+}
